23879df3ac76e87fd28c938520b0b146c644b390
nsano-rururu <nsano@ae.em-net.ne.jp>
2022-06-26 00:10:11 +0900

    Remove raven

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 import ElastalertServer from './elastalert_server';
 
-if (process.env.SENTRY_DSN !== undefined) {
-  var Raven = require('raven');
-  Raven.config(process.env.SENTRY_DSN, {
-    captureUnhandledRejections: true
-  }).install();
-  console.log('Sentry logging enabled for Elastalert');
-
-  Raven.context(function() {
-    let server = new ElastalertServer();
-    server.start();
-  });
-} else {
-  let server = new ElastalertServer();
-  server.start();
-}
+let server = new ElastalertServer();
+server.start();
